d49207301d37b20f43b1a72f078171155ce40d9f
lz-run <42399199+shengkelongshulong@users.noreply.github.com>
2019-01-24 11:45:43 +0800

    Update minigo.bash

--- minigo.bash
+++ minigo.bash
@@ -6,6 +6,7 @@ mv *.so.* /usr/lib/x86_64-linux-gnu
 
 echo -e "\n==========从google drive下载 "202"号 权重=========="
 gdown --id '1xSjAiLwG8PD8PLzXDRCWRTDDovS_3rnU' -O 'ffe8ba44feb8eb8ec8ff6d9b17ac075e4d708beadbfa68fee5506d431bbde68e.gz'
+echo -e "\n==========从google drive下载MINIGO 权重=========="
 gdown --id '1jldbQYIfA6jZhFn2QQ6SZGnr9iIzlY8y' -O 'minigo'
 mkdir networks && mv *.gz networks
 mkdir ~/.local/share/leela-zero
