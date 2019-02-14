53f2425533620cf8e7cd39d4eef31c9ca8e59999
lz-run <42399199+shengkelongshulong@users.noreply.github.com>
2019-02-14 09:04:03 +0800

    Update lz.bash

--- lz.bash
+++ lz.bash
@@ -4,7 +4,7 @@ gdown --id 1d6ZWhyLOVEh3WqcotjwTtu_bNGef3Ksl
 tar zxf lz0.16_colab.tgz
 mv *.so.* /usr/lib/x86_64-linux-gnu
 echo -e "\n==========从google drive下载elf 权重=========="
-gdown --id '1jOV2llKj9XLK8sXlBXa-eSeuxuQL57pz' -O 'v1'
+gdown --id '1kHXLDpdillw6UPJ9KHF93KVa2fG7HPoh' -O 'v2'
 echo -e "\n==========从google drive下载 "202"号 权重=========="
 gdown --id '1xSjAiLwG8PD8PLzXDRCWRTDDovS_3rnU' -O 'ffe8ba44feb8eb8ec8ff6d9b17ac075e4d708beadbfa68fee5506d431bbde68e.gz'
 echo -e "\n==========从google drive下载MINIGO 权重=========="
